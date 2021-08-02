b403776118e6951a1e48d992bef7b48d422acf97
Christopher Read <Christopherread22@outlook.com>
2021-08-02 18:23:49 -0400

    error check

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ useEffect(() => {
   axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
   .then(response => {
     setCoins(response.data)
-  }).catch(error => console.log(error))
+  }).catch(error => alert('ERROR'))
 }, []);
 
   return (
