62c4e4842c5b23cc5c808e57a58c6c97f2edb756
Christopher Read <Christopherread22@outlook.com>
2021-08-07 16:08:15 -0400

    priceChange

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Coin = ({name, image, symbol, price, volume}) => {
+const Coin = ({name, image, symbol, price, volume, priceChange}) => {
     return (
         <div className="coin-container">
             <div className="coin-row">
@@ -13,8 +13,8 @@ const Coin = ({name, image, symbol, price, volume}) => {
                     <p className="coin-price">${price}</p>
                     <p className="coin-volume">${volume.toLocaleString()}</p>
                     {priceChange < 0 ? (
-                        <p className="coin-percent red">{priceChange.toFixed(2)}</p>
-                    ) : (<p className="coin-percent green">{priceChange.toFixed(2)}</p>)
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                    ) : (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>)
                 }
                 </div>
             </div>
